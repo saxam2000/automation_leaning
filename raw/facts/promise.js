c8b97c762aa4afd759fb98dbf8e400f0b997f8a8
Saksham <saxamdhyani1999@gmail.com>
2021-04-03 11:16:23 +0530

    learned promise

--- raw/facts/promise.js
+++ raw/facts/promise.js
@@ -1,7 +1,26 @@
-let prom = new Promise((resolve, reject) => {
-    resolve("yayy.....");
-})
-const handlepromise = (resolvedvalue) => {
-    console.log(resolvedvalue)
+// let prom = new Promise((resolve, reject) => {
+//     resolve("yayy.....");
+// })
+// const handlepromise = (resolvedvalue) => {
+//     console.log(resolvedvalue)
+// }
+
+// prom.then(handlepromise)
+let fs = require("fs");
+
+function promisifiedreadfile(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, (err, data) => {
+            if (err) {
+                reject(err);
+            } else resolve(data);
+        })
+    })
 }
-prom.then(handlepromise)
+console.log("before")
+let frp = promisifiedreadfile("f1.txt");
+frp
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+// setTimeout(() => console.log(frp), 5000);
+console.log("after")
